9ad504239a5af1a9878743da26b137cdec1608c6
Daniel Zhang <68374769+xiaozhangup@users.noreply.github.com>
2026-02-08 21:22:53 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    val kotlinVersion = "1.8.0"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("com.github.gmazzo.buildconfig") version "3.1.0"
+    id("net.mamoe.mirai-console") version "2.16.0"
+}
+
+group = "org.example"
+version = "0.1.0"
+
+repositories {
+    if (System.getenv("CI")?.toBoolean() != true) {
+        maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
+    }
+    mavenCentral()
+}
+
+buildConfig {
+    className("BuildConstants")
+    packageName("org.example.mirai.plugin")
+    useKotlinOutput()
+
+    buildConfigField("String", "VERSION", "\"${project.version}\"")
+}
+
+mirai {
+    noTestCore = true
+    setupConsoleTestRuntime {
+        // 移除 mirai-core 依赖
+        classpath = classpath.filter {
+            !it.nameWithoutExtension.startsWith("mirai-core-jvm")
+        }
+    }
+}
+
+dependencies {
+    val overflowVersion = "1.0.6"
+    compileOnly("top.mrxiaom.mirai:overflow-core-api:$overflowVersion")
+    testConsoleRuntime("top.mrxiaom.mirai:overflow-core:$overflowVersion")
+}
